4dcff2932a7470960f19f4c25df4ac059091d29e
Gcenx <38226388+Gcenx@users.noreply.github.com>
2020-09-08 17:34:01 -0400

    Update unofficial-wineskin.rb

--- Casks/unofficial-wineskin.rb
+++ Casks/unofficial-wineskin.rb
@@ -4,7 +4,7 @@ cask 'unofficial-wineskin' do
   homepage "https://github.com/Gcenx/WineskinServer/"
   sha256 :no_check
   
-  url "https://github.com/Gcenx/WineskinServer/releases/download/V#{version}/Unofficial.Wineskin.Winery.-.No.compression.zip"
+  url "https://github.com/Gcenx/WineskinServer/releases/download/V#{version}/Unofficial.Wineskin.Winery.txz"
   
   name 'Unofficial Wineskin Winery'
 
