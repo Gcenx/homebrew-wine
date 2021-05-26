ec0bbb14ce64fdd548b40c171e805c7f9ec3b46a
Gcenx <38226388+Gcenx@users.noreply.github.com>
2021-05-26 15:51:18 -0400

    unofficial-wineskin.rb: revision 1
    Workaround https://github.com/Gcenx/WineskinServer/issues/96 until I’ve had chance to resolve it

--- Casks/unofficial-wineskin.rb
+++ Casks/unofficial-wineskin.rb
@@ -3,11 +3,18 @@ cask 'unofficial-wineskin' do
   version '1.8.4.2'
   homepage "https://github.com/Gcenx/WineskinServer/"
   sha256 :no_check
+  revision 1
   
   url "https://github.com/Gcenx/WineskinServer/releases/download/V#{version}/Wineskin.Winery.txz"
   
   name 'Wineskin Winery'
 
   app 'Wineskin Winery.app'
+
+  # Workaround issue until it's fixed in Winery
+  # https://github.com/Gcenx/WineskinServer/issues/96
+  preflight do
+    system_command "/bin/mkdir", args: ["-p", "~/Applications/Wineskin"], sudo: false
+  end
 
 end
