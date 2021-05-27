54606f5b156e007649c140011678616985b60aca
Dean M Greer <gcenx83@gmail.com>
2021-05-27 09:37:17 -0400

    unofficial-wineskin.rb: Correct preflight

--- Casks/unofficial-wineskin.rb
+++ Casks/unofficial-wineskin.rb
@@ -3,7 +3,6 @@ cask 'unofficial-wineskin' do
   version '1.8.4.2'
   homepage "https://github.com/Gcenx/WineskinServer/"
   sha256 :no_check
-  revision 1
   
   url "https://github.com/Gcenx/WineskinServer/releases/download/V#{version}/Wineskin.Winery.txz"
   
@@ -14,7 +13,7 @@ cask 'unofficial-wineskin' do
   # Workaround issue until it's fixed in Winery
   # https://github.com/Gcenx/WineskinServer/issues/96
   preflight do
-    system_command "/bin/mkdir", args: ["-p", "~/Applications/Wineskin"], sudo: false
+    system_command "/bin/mkdir", args: ["-p", "/Users/#{ENV["USER"]}/Applications/Wineskin"], sudo: false
   end
 
 end
