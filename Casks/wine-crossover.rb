f5ba54dd225b98873f7eada2434ef151c3c07497
Dean M Greer <gcenx83@gmail.com>
2021-12-05 23:23:21 -0500

    wine-crossover: 21.0.0 rebuild 2
    Require macOS Catalina and later.

--- Casks/wine-crossover.rb
+++ Casks/wine-crossover.rb
@@ -2,15 +2,15 @@ cask 'wine-crossover' do
 
   version '21.0.0'
   homepage "https://github.com/Gcenx/homebrew-wine/"
-  sha256 "54986c1f1011c7dc336cf09782fbd254e61cdb03dcb301d32db3c3b669edcf94"
-  
+  sha256 "f5fcbe98977cc42ba39813140bcde45e10b9e0bd24da435a6a7227bb62fd0100"
+
   url "https://github.com/Gcenx/homebrew-wine/releases/download/#{version}/wine-crossover-#{version}-osx64.tar.xz"
   name 'Wine Crossover'
-  
+
   depends_on formula: 'xz'
-  
-  depends_on macos: ">= :high_sierra"
-  
+
+  depends_on macos: ">= :catalina"
+
   conflicts_with formula: 'wine',
                  cask:    [
                             'wine-stable',
@@ -27,6 +27,7 @@ cask 'wine-crossover' do
   binary "#{appdir}/Wine Crossover.app/Contents/Resources/wine/bin/notepad"
   binary "#{appdir}/Wine Crossover.app/Contents/Resources/wine/bin/regedit"
   binary "#{appdir}/Wine Crossover.app/Contents/Resources/wine/bin/regsvr32"
+  binary "#{appdir}/Wine Crossover.app/Contents/Resources/wine/bin/wine"
   binary "#{appdir}/Wine Crossover.app/Contents/Resources/wine/bin/wine64"
   binary "#{appdir}/Wine Crossover.app/Contents/Resources/wine/bin/wineboot"
   binary "#{appdir}/Wine Crossover.app/Contents/Resources/wine/bin/winecfg"
@@ -36,8 +37,6 @@ cask 'wine-crossover' do
   binary "#{appdir}/Wine Crossover.app/Contents/Resources/wine/bin/winemine"
   binary "#{appdir}/Wine Crossover.app/Contents/Resources/wine/bin/winepath"
   binary "#{appdir}/Wine Crossover.app/Contents/Resources/wine/bin/wineserver"
-  binary "#{appdir}/Wine Crossover.app/Contents/Resources/start/bin/wine"
-  binary "#{appdir}/Wine Crossover.app/Contents/Resources/wine/bin/wine32on64"
 
     caveats <<~EOS
         #{token} supports both 32-bit and 64-bit now. It is compatible with your
@@ -45,7 +44,7 @@ cask 'wine-crossover' do
         create a new wine prefix. The architecture can be selected using the
           WINEARCH environment variable which can be set to either win32 or
         win64.
-      
+
         To create a new pure 32-bit prefix, you can run:
             $ WINEARCH=win32 WINEPREFIX=~/.wine32 winecfg
         See the Wine FAQ for details: https://wiki.winehq.org/FAQ#Wineprefixes
