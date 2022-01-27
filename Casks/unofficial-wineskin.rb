d71effa14841192a7a7f55900f0bf984f717c2c4
Gcenx <38226388+Gcenx@users.noreply.github.com>
2022-01-26 22:41:53 -0500

    cleanup casks

--- Casks/unofficial-wineskin.rb
+++ Casks/unofficial-wineskin.rb
@@ -1,6 +1,17 @@
+<<<<<<< HEAD
 cask "unofficial-wineskin" do
   version "1.8.4.2"
   sha256 :no_check
+=======
+cask 'unofficial-wineskin' do
+  version '1.8.4.2'
+  sha256 :no_check
+
+  url "https://github.com/Gcenx/WineskinServer/releases/download/V#{version}/Wineskin.Winery.txz"
+  name 'Wineskin Winery'
+  desc "Porting tool, to make Windows programs/games into MacOS apps. GUI building, made for ease of use and customization."
+  homepage "https://github.com/Gcenx/WineskinServer/"
+>>>>>>> cd3fe3f (cleanup casks)
 
   url "https://github.com/Gcenx/WineskinServer/releases/download/V#{version}/Wineskin.Winery.txz"
   name "Wineskin Winery"
