255bc9415301ccc299e656ccf5ec1e98aa97a937
Dean M Greer <gcenx83@gmail.com>
2022-02-16 20:29:15 -0500

    cx-llvm 8.0 (new formula)
    Closes #40.
    
    Signed-off-by: Dean M Greer <38226388+Gcenx@users.noreply.github.com>

---
+++ Formula/cx-llvm.rb
@@ -0,0 +1,62 @@
+class CxLlvm < Formula
+  desc "CodeWeavers custom compiler for -mwine32 targets"
+  homepage "https://codeweavers.com/"
+  url "https://media.codeweavers.com/pub/crossover/source/crossover-sources-21.1.0.tar.gz"
+  version "8.0" # based on llvm/clang 8.0
+  sha256 "fc96ba12b2694ec1381983154941a06619adda5d4b805b1c0d7d95a1040b58dd"
+  license "NCSA"
+
+  # Clang cannot find system headers if Xcode CLT is not installed
+  pour_bottle? only_if: :clt_installed
+
+  keg_only :versioned_formula
+
+  # https://llvm.org/docs/GettingStarted.html#requirement
+  depends_on "cmake" => :build
+  depends_on xcode: :build
+  depends_on arch: :x86_64
+
+  uses_from_macos "libedit"
+  uses_from_macos "libffi"
+  uses_from_macos "libxml2"
+  uses_from_macos "ncurses"
+  uses_from_macos "zlib"
+
+  def install
+    mv "clang/clang", "clang/llvm/projects/clang"
+    mv "clang/llvm", "llvm"
+
+    # Apple's libstdc++ is too old to build LLVM
+    ENV.libcxx if ENV.compiler == :clang
+
+    # compiler-rt has some iOS simulator features that require i386 symbols
+    # I'm assuming the rest of clang needs support too for 32-bit compilation
+    # to work correctly, but if not, perhaps universal binaries could be
+    # limited to compiler-rt. llvm makes this somewhat easier because compiler-rt
+    # can almost be treated as an entirely different build from llvm.
+    ENV.permit_arch_flags
+
+    args = %W[
+      -DLIBOMP_ARCH=x86_64
+      -DFFI_INCLUDE_DIR=#{MacOS.sdk_path}/usr/include/ffi
+      -DFFI_LIBRARY_DIR=#{MacOS.sdk_path}/usr/lib
+    ]
+
+    if MacOS.version >= :mojave
+      sdk_path = MacOS::CLT.installed? ? "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk" : MacOS.sdk_path
+      args << "-DDEFAULT_SYSROOT=#{sdk_path}"
+    end
+
+    cd "llvm" do
+      mkdir "build" do
+        system "cmake", "-G", "Unix Makefiles", "..", *(std_cmake_args + args)
+        system "make"
+        system "make", "install"
+      end
+    end
+  end
+
+  test do
+    system "#{bin}/clang", "--version"
+  end
+end
