4efe49a838ff1e10d07afc7b2fa268a5958b55a9
Dean M Greer <38226388+Gcenx@users.noreply.github.com>
2022-05-12 18:11:20 -0400

    wine: remove revision bump

--- Formula/wine.rb
+++ Formula/wine.rb
@@ -8,7 +8,6 @@ class Wine < Formula
   desc "Run Windows applications without a copy of Microsoft Window"
   homepage "https://www.winehq.org/"
   license "GPL-2.0-or-later"
-  revision 1
   head "https://source.winehq.org/git/wine.git", branch: "master"
 
   stable do
