6cf8338a3976e06435f772c646e2547435abd5f7
Torrekie <me@torrekie.dev>
2022-08-26 03:55:20 +0800

    add tapi formulae

---
+++ Formula/tapi.rb
@@ -0,0 +1,60 @@
+class Tapi < Formula
+  desc "Apple TAPI library (tpoechtrager fork)"
+  homepage "https://opensource.apple.com/tarballs/tapi"
+  url "https://github.com/tpoechtrager/apple-libtapi/archive/1100.0.11.tar.gz"
+  sha256 "bef360e713852f451383a7f1fdd4abfd17ee2d937778cd757708e52595d77b78"
+  license "APSL-2.0"
+
+  depends_on "cmake" => :build
+
+  patch :p0, :DATA
+
+  def install
+    ENV.cxx11
+
+    append_includes = ["-I#{buildpath}/src/llvm/projects/clang/include -I#{buildpath}/build/projects/clang/include"]
+
+    args = %W[
+      -DCMAKE_CXX_FLAGS=#{append_includes.join(" ")}
+      -DLLVM_ENABLE_PROJECTS=libtapi
+      -DLLVM_ENABLE_TERMINFO=OFF
+      -DLLVM_INCLUDE_TESTS=OFF
+      -DTAPI_FULL_VERSION=#{version}
+      -DTAPI_INCLUDE_DOCS=ON
+      -DTAPI_INCLUDE_TESTS=OFF
+    ]
+    args << "-DPYTHON_EXECUTABLE=/usr/bin/python3" if MacOS.version > :catalina
+    args << "-DPYTHON_EXECUTABLE=/usr/bin/python2.7" if MacOS.version <= :catalina
+
+    mkdir "build" do
+      system "cmake", "-G", "Unix Makefiles", "../src/llvm",
+             *args, *std_cmake_args
+      system "make", "libtapi", "tapi"
+      system "make", "install-libtapi", "install-tapi-headers", "install-tapi",
+                     "install-tapi-clang-headers", "install-tapi-docs"
+    end
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test tapi`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
+__END__
+diff --git src/llvm/projects/libtapi/CMakeLists.txt src/llvm/projects/libtapi/CMakeLists.txt
+index 8ee6d8138..5daf0a5fb 100644
+--- src/llvm/projects/libtapi/CMakeLists.txt
++++ src/llvm/projects/libtapi/CMakeLists.txt
+@@ -203,3 +203,4 @@ get_property(TAPI_TABLEGEN_TARGETS GLOBAL PROPERTY TAPI_TABLEGEN_TARGETS)
+ list(APPEND LLVM_COMMON_DEPENDS ${TAPI_TABLEGEN_TARGETS})
+ add_subdirectory(lib)
+ add_subdirectory(tools)
++add_subdirectory(docs)
