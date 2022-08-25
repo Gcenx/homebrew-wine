50668ffd3b0ac90353c92f74a2d1264f38ff348d
Torrekie <me@torrekie.dev>
2022-08-26 03:57:36 +0800

    fix test

--- Formula/tapi.rb
+++ Formula/tapi.rb
@@ -36,16 +36,7 @@ class Tapi < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test tapi`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "#{bin}/tapi", "--version"
   end
 end
 __END__
