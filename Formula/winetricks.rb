62212b74ff86aa736100acb66e6670736e7badb8
Dean M Greer <38226388+Gcenx@users.noreply.github.com>
2022-10-23 20:48:29 +0000

    winetricks: add 20221022 bottle.

--- Formula/winetricks.rb
+++ Formula/winetricks.rb
@@ -11,6 +11,11 @@ class Winetricks < Formula
     regex(/^v?(\d{6,8})$/i)
   end
 
+  bottle do
+    root_url "https://github.com/Gcenx/homebrew-wine/releases/download/winetricks-20221022"
+    sha256 cellar: :any_skip_relocation, big_sur: "69b0b05ed069ce4760ca1f0b34ec42bebb72684853bf2bbfb0fc5cd6e0948308"
+  end
+
   depends_on "cabextract"
   depends_on "p7zip"
   depends_on "unzip"
