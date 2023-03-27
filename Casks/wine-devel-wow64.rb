335788d46bbbbf26e9e9d5d0a96cd4f17b77fb6e
Dean M Greer <38226388+Gcenx@users.noreply.github.com>
2023-03-27 10:54:29 -0400

    Create wine-devel-wow64.rb

---
+++ Casks/wine-devel-wow64.rb
@@ -0,0 +1,56 @@
+cask "wine-devel-wow64" do
+  version "8.4"
+  sha256 "1f404a755907655a4f201f7ac092ba22c10d4f19ca630afe3b7c44776f0fba02"
+
+  # Current winehq packages are deprecated and these are packages from
+  # the new maintainers that will eventually be pushed to Winehq.
+  # See https://www.winehq.org/pipermail/wine-devel/2021-July/191504.html
+  url "https://github.com/Gcenx/macOS_Wine_builds/releases/download/#{version}/wine-devel-#{version}-wow64-osx64.tar.xz",
+      verified: "github.com/Gcenx/macOS_Wine_builds/"
+  name "WineHQ-devel"
+  desc "Compatibility layer to run Windows applications"
+  homepage "https://wiki.winehq.org/MacOS"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  conflicts_with formula: "wine",
+                 cask:    %w[
+                   wine-crossover
+                   wine-stable
+                   wine-stable-legacy
+                   wine-devel
+                   wine-staging
+                 ]
+  depends_on macos: ">= :catalina"
+
+  app "Wine Devel.app"
+  binary "#{appdir}/Wine Devel.app/Contents/Resources/start/bin/appdb"
+  binary "#{appdir}/Wine Devel.app/Contents/Resources/start/bin/winehelp"
+  binary "#{appdir}/Wine Devel.app/Contents/Resources/wine/bin/msiexec"
+  binary "#{appdir}/Wine Devel.app/Contents/Resources/wine/bin/notepad"
+  binary "#{appdir}/Wine Devel.app/Contents/Resources/wine/bin/regedit"
+  binary "#{appdir}/Wine Devel.app/Contents/Resources/wine/bin/regsvr32"
+  binary "#{appdir}/Wine Devel.app/Contents/Resources/wine/bin/wine"
+  binary "#{appdir}/Wine Devel.app/Contents/Resources/wine/bin/wine64"
+  binary "#{appdir}/Wine Devel.app/Contents/Resources/wine/bin/wineboot"
+  binary "#{appdir}/Wine Devel.app/Contents/Resources/wine/bin/winecfg"
+  binary "#{appdir}/Wine Devel.app/Contents/Resources/wine/bin/wineconsole"
+  binary "#{appdir}/Wine Devel.app/Contents/Resources/wine/bin/winedbg"
+  binary "#{appdir}/Wine Devel.app/Contents/Resources/wine/bin/winefile"
+  binary "#{appdir}/Wine Devel.app/Contents/Resources/wine/bin/winemine"
+  binary "#{appdir}/Wine Devel.app/Contents/Resources/wine/bin/winepath"
+  binary "#{appdir}/Wine Devel.app/Contents/Resources/wine/bin/wineserver"
+
+  caveats <<~EOS
+    #{token} supports running 32-bit & 64-bit windows binaries.
+    #{token} does not support creating a 32-bit wine prefix.
+  EOS
+  caveats <<~EOS
+    To enable noflicker set the following registry key in your prefix:
+    [HKCU\\Software\\Wine\\Mac Driver]
+    "ForceOpenGLBackingStore"="y"
+  EOS
+end
