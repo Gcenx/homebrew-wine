7caab8f9610398682d4390140a7020ed11d6335d
Dean M Greer <38226388+Gcenx@users.noreply.github.com>
2024-10-11 14:49:19 -0400

    Replace wineskin with winery (#152)
    Replace Wineskin with Kegworks

---
+++ Casks/kegworks.rb
@@ -0,0 +1,29 @@
+cask "kegworks" do
+  version "2.0.3"
+  sha256 "bc30236554c7333f35fbff78b7eb849854b5c9521a917007cc9b4944421c5b6a"
+
+  url "https://github.com/Kegworks-App/Winery/releases/download/v#{version}/winery-v#{version}.tar.xz"
+  name "Winery"
+  desc "Porting tool, to make Windows programs/games into native apps"
+  homepage "https://github.com/Kegworks-App"
+
+  depends_on macos: ">= :catalina"
+
+  app "Winery.app", target: "Kegworks Winery.app"
+
+  postflight do
+    system "/usr/bin/xattr", "-drs", "com.apple.quarantine", "#{appdir}/Winery.app"
+    system "/usr/bin/codesign", "--force", "--deep", "-s", "-", "#{appdir}/Winery.app"
+    system_command "/bin/mkdir", args: ["-p", "/Users/#{ENV.fetch("USER")}/Applications/Kegworks"], sudo: false
+  end
+
+  zap trash: [
+    "~/Library/Application Support/Kegworks",
+    "~/Library/Caches/com.unofficial.winery",
+    "~/Library/Caches/com.unofficial.wineskin",
+  ]
+
+  caveats do
+    requires_rosetta
+  end
+end
