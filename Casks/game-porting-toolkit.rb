0b8d18219faabc3bf1f2a270d49211aa6bf670ee
Yihe Li <winmikedows@hotmail.com>
2026-09-06 08:23:52 +0800

    Replace postflight with postflight_steps

--- Casks/game-porting-toolkit.rb
+++ Casks/game-porting-toolkit.rb
@@ -25,7 +25,7 @@ cask "game-porting-toolkit" do
   binary "#{appdir}/Game Porting Toolkit.app/Contents/Resources/wine/bin/wine64-preloader"
   binary "#{appdir}/Game Porting Toolkit.app/Contents/Resources/wine/bin/wineserver"
 
-  postflight do
+  postflight_steps do
     system "/usr/bin/xattr", "-drs", "com.apple.quarantine", "#{appdir}/Game Porting Toolkit.app"
     system "/usr/bin/codesign", "--force", "--deep", "-s", "-", "#{appdir}/Game Porting Toolkit.app"
   end
